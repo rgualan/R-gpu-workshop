a83f30c294e1e6027855643d3a66f96c583ff873
Unknown <rmgs1u16@soton.ac.uk>
2018-07-08 22:32:49 -0500

    Agregar ejemplo OLS solve
    Usando gpuR

--- gpuR_example.R
+++ gpuR_example.R
@@ -5,32 +5,41 @@ library(ggplot2)
 # entre la operacion regular (usando el CPU)
 # y la operacion implementada por la libreria gpuR (usando la GPU)
 
-orders <- seq(1000,4000,by=1000)
-t1 <- t2 <- sizes <- c()
+#orders <- seq(1000,4000,by=1000)
+orders <- seq(1000,2000,by=500)
+t1 <- t2 <- t3 <- sizes <- c()
 
 for(order in orders) {
   print(order)
   A = matrix(rnorm(order^2), nrow=order) 
   sizes <- c( sizes, format(object.size(A), units="auto") )
-  gpuA = gpuMatrix(A, type="float")
+
+  # GPU version, GPU pointer to CPU memory!! (gpuMatrix is simply a pointer)
+  gpuA = gpuMatrix(A, type="float") #point GPU to matrix
   
+  # GPU version, in GPU memory!! (vclMatrix formation is a memory transfer)
+  vclA = vclMatrix(A, type="float") #push matrix to GPU
+
   print("cpu...")
   elapsed1 <- system.time({C = A %*% A})[3]  # Operacion regular (ejecutada en la CPU) 
   print("gpu...")
   elapsed2 <- system.time({gpuC = gpuA %*% gpuA})[3] # Operacion acelerada (ejecutada en la GPU)
+  print("gpu2...")
+  elapsed3 <- system.time({vclC = vclA %*% vclA})[3] # Operacion acelerada (ejecutada en la GPU)
   
   t1 = c(t1, elapsed1)
   t2 = c(t2, elapsed2)
+  t3 = c(t3, elapsed3)
   
   # Liberar espacio en memoria:
-  rm(A,C,gpuA,gpuC); gc()
+  rm(A,C,gpuA,gpuC,vclA,vclC); gc()
 }
 
 # Armar un dataframe con los resultados del bucle
-df = data.frame(order = rep(orders,2), 
-                label = rep(paste0(orders," (", sizes, ")"), 2),
-                mode = rep(c("cpu","gpu"),each=length(orders)), 
-                elapsed = c(t1,t2))
+df = data.frame(order = rep(orders,3), 
+                label = rep(paste0(orders," (", sizes, ")"), 3),
+                mode = rep(c("cpu","gpu","gpu-2"),each=length(orders)), 
+                elapsed = c(t1,t2,t3))
 
 # Guardar el dataframe (ya que el codigo de este chunk para plotearlo a continuación)
 saveRDS(df, file = "data/gpuMm.rds")
@@ -46,7 +55,7 @@ if(F){
   library(profvis)
   order = 2000
   A = matrix(rnorm(order^2), nrow=order) 
-  gpuA = gpuMatrix(A, type="float")
+  gpuA = gpuMatrix(A, type="float") 
   
   profvis({C = A %*% A})[3]  # Operacion regular (ejecutada en la CPU) 
   profvis({gpuC = gpuA %*% gpuA})[3] # Operacion acelerada (ejecutada en la GPU)
---
+++ gpuR_ols_solve.R
@@ -0,0 +1,47 @@
+# OLS solve
+# Ordinary Least Squares
+
+set.seed(123456)
+np <- 300  #number of predictors (30)
+nr <- 1e+05  #number of observations
+X <- cbind(5, 1:nr, matrix(rnorm((np - 1) * nr, 0, 0.01), nrow = nr, ncol = (np - 1)))
+beta <- matrix(c(1, 3, runif(np - 1, 0, 0.2)), ncol = 1)
+y <- X %*% beta + matrix(rnorm(nr, 0, 1), nrow = nr, ncol = 1)
+
+# CPU bound version, slight optimize via crossprod but otherwise vanilla
+timeCpu <- system.time({
+  ms2 <- solve(crossprod(X), crossprod(X, y))
+})
+
+library(gpuR)
+# GPU version, GPU pointer to CPU memory!! (gpuMatrix is simply a pointer)
+gpuX = gpuMatrix(X, type = "float")  #point GPU to matrix
+gpuy = gpuMatrix(y, type = "float")
+timeGpu <- system.time({
+  ms4 <- gpuR::solve(gpuR::crossprod(gpuX), gpuR::crossprod(gpuX, gpuy))
+})
+
+
+# GPU version, in GPU memory!! (vclMatrix formation is a memory transfer)
+vclX = vclMatrix(X, type = "float")  #push matrix to GPU
+vcly = vclMatrix(y, type = "float")
+timeGpu2 <- system.time({
+  ms5 <- gpuR::solve(gpuR::crossprod(vclX), gpuR::crossprod(vclX, vcly))
+})
+
+detach("package:gpuR", unload = TRUE)
+
+
+timeCpu
+timeGpu
+timeGpu2
+
+# Armar un dataframe con los resultados del bucle
+df = data.frame(calculo = c("CPU", "GPU (gpu*)", "GPU (vcl*)"), 
+                tiempo = c(timeCpu[3], timeGpu[3], timeGpu2[3]) )
+
+# Plotear los resultados
+ggplot(df, aes(x=calculo, y=tiempo, fill=calculo)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  xlab("Tipo de calculo") + 
+  ylab("Tiempo de ejecucion (seg)") #+ scale_y_log10() 
